e310b351d25b7ab5665894df783b8be95c396ac2
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-07-21 11:49:10 -0400

    Finish authentication with downcase on email before validation on users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,5 @@ require 'spec_helper'
 
 describe User do
   it { is_expected.to have_many(:tweets) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_presence_of(:tag) }
-  it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_uniqueness_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:tag) }
 end
