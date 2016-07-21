aa51f80a1565d7a457745e979becccecf65eb124
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-07-21 18:44:19 -0400

    Finish tweet to user association

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,17 +2,33 @@ require 'spec_helper'
 
 describe TweetsController do
   describe "POST create" do
+    it "redirects to the sign in page for unauthenticated users" do
+      post :create, tweet: { body: "My first tweet :)" }
+      expect(response).to redirect_to sign_in_path
+    end
+    
     context "with valid input" do
-      before { post :create, tweet: { body: "My first tweet :)" } }
+      let(:user) { Fabricate(:user) }
+      before do 
+        session[:user_id] = user.id
+        post :create, tweet: { body: "My first tweet :)" }
+      end
       it "creates a new tweet" do
         expect(Tweet.count).to eq(1)
       end
       it "redirects the home page" do
         expect(response).to redirect_to home_path
       end
+      it "associates the tweet with the signed in user" do
+        expect(user.tweets).to include(Tweet.first)
+      end
     end
     context "with invalid input" do
-      before { post :create, tweet: { body: "" } }
+      let(:user) { Fabricate(:user) }
+      before do 
+        session[:user_id] = user.id
+        post :create, tweet: { body: "" }
+      end
       it "doesn't create a new tweet" do
         expect(Tweet.count).to eq(0)
       end
