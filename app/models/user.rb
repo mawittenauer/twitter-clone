28b23f02ef643aa1bc79e365dc00e37c75557d4a
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-08-11 11:41:20 -0400

    Add mentions to user model and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   before_validation { downcase_email }
   
   has_many :tweets
+  has_many :mentions, class_name: 'Mention', foreign_key: 'mentioned_id'
   has_secure_password validation: false
   validates_presence_of :email, :tag, :password
   validates_uniqueness_of :email, :tag
