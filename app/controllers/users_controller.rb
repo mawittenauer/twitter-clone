d24acf1ed9630f9db530048a2bd113ea4cb3600e
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-08-24 19:42:32 -0400

    Add avatar to user_params on users controller:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:tag, :email, :password)
+    params.require(:user).permit(:tag, :email, :password, :avatar)
   end
 end
