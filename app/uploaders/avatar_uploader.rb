f8dc785914d012390f59171c68b6ac1774813742
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-08-25 08:35:00 -0400

    Add uploader to user model

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,4 +2,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   
   process :resize_to_fill => [200, 200]
+  
+  def default_url(*args)
+    "/images/default_profile_pic.jpg"
+  end
 end
