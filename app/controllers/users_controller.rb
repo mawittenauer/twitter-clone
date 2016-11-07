7e04aef84f699387457b9bae6f23255dd9303f55
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-11-07 10:25:11 -0500

    Add basic index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :require_user, only: [:index]
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
     @tweets = @user.tweets
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#front'
   get '/home', to: 'pages#home'
   resources :tweets, only: [:create]
-  resources :users, only: [:create, :show] do
+  resources :users, only: [:create, :show, :index] do
     resources :relationships, only: [:create]
     member do
       delete '/relationships', to: 'relationships#destroy'
