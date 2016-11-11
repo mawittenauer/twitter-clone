1d5facfe6e5c7a56eacbc3720dcc74e7ae9197cd
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-11-11 09:59:51 -0500

    Add pagination to search results page and controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,6 @@ class SearchController < ApplicationController
   before_action :require_user
   
   def user_search
-    @results = Search.user_search(params[:search_term])
+    @results = Search.user_search(params[:search_term]).paginate(:page => params[:page], :per_page => 30)
   end
 end
