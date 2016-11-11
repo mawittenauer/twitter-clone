093a43a048847ba0e9b314633b93fe96afe2872d
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-11-11 09:01:23 -0500

    remove user model test error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe User do
   it { is_expected.to have_many(:tweets) }
-  it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to have_many(:mentions) }
   
   describe "#user_feed" do
